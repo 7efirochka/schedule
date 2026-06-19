b5864b0e32c82dd0d2366bcc10bee272f599b2f7
EPF <efimchenkov@ya.ru>
2026-06-19 12:11:10 +0000

    add dev server to nuxt conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,9 @@ export default defineNuxtConfig(
   compatibilityDate: '2026-06-03',
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   modules: ['@pinia/nuxt'],
+
+  devServer: {
+    host: 'timesheet.atlas-pro24.local', // Listens on all local IP addresses
+    port: 3000       // Optional: defaults to 3000
+  }
 })
